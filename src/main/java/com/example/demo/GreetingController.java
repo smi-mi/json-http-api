078d6b994267daa012ff2c8b3af8dc1278ae677e
Mariia <smimi_00@mail.ru>
2020-07-06 14:20:57 +0300

    WIP: refactoring class diagram

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -21,7 +21,7 @@ public class GreetingController {
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
 
-        User n = new User();
+        Client n = new Client();
         n.setName(name);
         n.setEmail(email);
         userRepository.save(n);
@@ -29,7 +29,7 @@ public class GreetingController {
     }
 
     @GetMapping(path="/all")
-    public @ResponseBody Iterable<User> getAllUsers() {
+    public @ResponseBody Iterable<Client> getAllUsers() {
         // This returns a JSON or XML with the users
         return userRepository.findAll();
     }
