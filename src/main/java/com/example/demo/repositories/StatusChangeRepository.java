48d922d7c8aa0223a18d9716dad81cdeeadc236c
Mariia <smimi_00@mail.ru>
2020-07-08 21:31:08 +0300

    finished statistic service test

--- src/main/java/com/example/demo/repositories/StatusChangeRepository.java
+++ src/main/java/com/example/demo/repositories/StatusChangeRepository.java
@@ -3,18 +3,19 @@ package com.example.demo.repositories;
 import com.example.demo.entities.Status;
 import com.example.demo.entities.StatusChange;
 import com.example.demo.repositories.projections.ProfileOnly;
+import lombok.NonNull;
 import org.springframework.data.repository.CrudRepository;
 
 public interface StatusChangeRepository extends CrudRepository<StatusChange, Integer> {
 
     Iterable<ProfileOnly> findDistinctByTimestampGreaterThanAndProfileStatusLike(
-            Long time,
-            Status status
+            @NonNull Long time,
+            @NonNull Status status
     );
 
-    Iterable<ProfileOnly> findDistinctByTimestampGreaterThan(Long time);
+    Iterable<ProfileOnly> findDistinctByTimestampGreaterThan(@NonNull Long time);
 
-    Iterable<ProfileOnly> findDistinctByProfileStatusLike(Status status);
+    Iterable<ProfileOnly> findDistinctByProfileStatusLike(@NonNull Status status);
 
     //Iterable<ProfileOnly> findDistinctAll();
 }
--- src/test/java/com/example/demo/services/StatisticServiceTest.java
+++ src/test/java/com/example/demo/services/StatisticServiceTest.java
@@ -1,16 +1,15 @@
 package com.example.demo.services;
 
+import com.example.demo.entities.Profile;
 import com.example.demo.entities.Status;
 import com.example.demo.repositories.StatusChangeRepository;
 import com.example.demo.repositories.StatusRepository;
 import com.example.demo.repositories.projections.ProfileOnly;
-import com.example.demo.services.impl.StatisticServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -48,61 +47,79 @@ public class StatisticServiceTest {
                 .thenReturn(Optional.empty());
     }
 
-    @BeforeEach
-    public void initStatusChangeRepository() {
-        Mockito.when(statusChangeRepository.findDistinctByProfileStatusLike(null))
-                .thenThrow(IllegalArgumentException.class);
-        Mockito.when(statusChangeRepository.findDistinctByTimestampGreaterThan(null))
-                .thenThrow(IllegalArgumentException.class);
-        Mockito.when(statusChangeRepository.findDistinctByTimestampGreaterThanAndProfileStatusLike(
-                null, null))
-                .thenThrow(IllegalArgumentException.class);
-    }
-
     @Test
     public void get_allParamsIsNull_returnAllStatusChanges() {
-        Iterable<ProfileOnly> statusChangeResponse = List.of();
+        Iterable<ProfileOnly> statusChangeResponse = List.of(
+                new ProfileOnly(new Profile()),
+                new ProfileOnly(new Profile()),
+                new ProfileOnly(new Profile())
+        );
         Mockito.when(statusChangeRepository.findDistinctByTimestampGreaterThan(any()))
                 .thenReturn(statusChangeResponse);
+
         Iterable<ProfileOnly> response = statisticService.get(null, null);
-        Assertions.assertEquals(response, statusChangeResponse);
+
+        Assertions.assertEquals(statusChangeResponse, response);
     }
 
     @Test
     public void get_statusValueIsNull_returnFilteredByTime() {
-        Iterable<ProfileOnly> statusChangeResponse = List.of();
+        Iterable<ProfileOnly> statusChangeResponse = List.of(
+                new ProfileOnly(new Profile()),
+                new ProfileOnly(new Profile())
+        );
         Mockito.when(statusChangeRepository.findDistinctByTimestampGreaterThan(any()))
                 .thenReturn(statusChangeResponse);
-        Iterable<ProfileOnly> response = statisticService.get(null, 1000L);
-        Assertions.assertEquals(response, statusChangeResponse);
+
+        final Long timestamp = 1000L;
+        Iterable<ProfileOnly> response = statisticService.get(null, timestamp);
+
+        Assertions.assertEquals(statusChangeResponse, response);
     }
 
     @Test
     public void get_statusValueIsOnline_returnFilteredByStatus() {
-        Iterable<ProfileOnly> onlines = List.of();
-        Status online = new Status("Online");
-        Mockito.when(statusChangeRepository.findDistinctByProfileStatusLike(online))
+        Iterable<ProfileOnly> onlines = List.of(
+                new ProfileOnly(new Profile()),
+                new ProfileOnly(new Profile())
+        );
+        Mockito.when(statusChangeRepository.findDistinctByProfileStatusLike(
+                Mockito.argThat(arg -> arg.getValue().equals("Online"))))
                 .thenReturn(onlines);
-        Assertions.assertEquals(statisticService.get("Online", null), onlines);
+
+        Assertions.assertEquals(onlines, statisticService.get("Online", null));
     }
 
     @Test
     public void get_statusValueIsOffline_returnFilteredByStatus() {
-        Iterable<ProfileOnly> offlines = List.of();
+        Iterable<ProfileOnly> offlines = List.of(new ProfileOnly(new Profile()));
         Mockito.when(statusChangeRepository.findDistinctByProfileStatusLike(
                 Mockito.argThat(arg -> arg.getValue().equals("Offline"))))
                 .thenReturn(offlines);
-        Assertions.assertEquals(statisticService.get("Offline", null), offlines);
+
+        Assertions.assertEquals(offlines, statisticService.get("Offline", null));
     }
 
     @Test
     public void get_badStatusValue_throwNoSuchElementException() {
         Assertions.assertThrows(NoSuchElementException.class,
-                () -> statisticService.get("BadStatus", null));
+                () -> statisticService.get("BadStatus", null)
+        );
     }
 
     @Test
     public void get_allParamsIsNotNull_returnFilteredByStatusAndTime() {
-        // TODO
+        Iterable<ProfileOnly> expectedResponse = List.of(
+                new ProfileOnly(new Profile()),
+                new ProfileOnly(new Profile()),
+                new ProfileOnly(new Profile())
+        );
+        Mockito.when(statusChangeRepository.findDistinctByTimestampGreaterThanAndProfileStatusLike(
+                Mockito.anyLong(), any()))
+                .thenReturn(expectedResponse);
+
+        Assertions.assertEquals(expectedResponse,
+                statisticService.get("Online", 100L)
+        );
     }
 }
